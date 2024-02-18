07e88e4af90a58edbbc29208fcce41e4d4ef2593
ajin <aj5210@naver.com>
2024-02-18 13:19:42 +0900

    :recycle: Refactor(paging semester): getindexlist action 분리

--- src/components/paging-semester.tsx
+++ src/components/paging-semester.tsx
@@ -1,6 +1,8 @@
 'use client'
 
-import { period } from '@/lib/data'
+import { useEffect } from 'react'
+
+import { useSemesterStore } from '@/store/semester'
 import {
   Pagination,
   PaginationContent,
@@ -9,25 +11,9 @@ import {
   PaginationPrevious,
 } from '@/components/ui/pagination'
 import { PagingSemesterItems } from '@/components/paging-semester-items'
-import { useSemesterStore } from '@/store/semester'
-import { useEffect } from 'react'
 
 export const PagingSemester = () => {
-  const { semester, currentIndex, setCurrentIndex, setPreviousSemester, setNextSemester } = useSemesterStore()
-
-  const getIndexList = (index: number, range: number) => {
-    let start = Math.max(0, index - range)
-    let end = Math.min(period.length - 1, index + range)
-
-    if (currentIndex < range) {
-      end = range * 2
-    }
-    if (currentIndex >= period.length - range) {
-      start = (period.length - 1) - range * 2
-    }
-    
-    return period.slice(start, end + 1)
-  }
+  const { semester, currentIndex, setCurrentIndex, setPreviousSemester, setNextSemester, getIndexList } = useSemesterStore()
 
   useEffect(() => {
     const index = useSemesterStore.getState().getSemesterIndex(semester)
--- src/store/semester.ts
+++ src/store/semester.ts
@@ -1,41 +1,59 @@
-import { period } from '@/lib/data'
 import { create } from 'zustand'
 import { devtools } from 'zustand/middleware'
 
+import { period } from '@/lib/data'
+
 interface semesterProps {
   semester: string
-  setSemester: (value: string) => void
   currentIndex: number
+  setSemester: (value: string) => void
   setCurrentIndex: (index: number) => void
   setPreviousSemester: () => void
   setNextSemester: () => void
+  getIndexList: (index: number, range: number) => string[]
   getSemesterIndex: (semester: string) => number
 }
 
 export const useSemesterStore = create<semesterProps>()(
   devtools((set) => ({
-    semester: '2023-2',
+    semester: period[1],
     currentIndex: 1,
     setSemester: (value) => set(() => ({ semester: value })),
+    setCurrentIndex: (index) => set(() => ({ currentIndex: index})),
     setPreviousSemester: () => {
       set((value) => {
         const index = value.getSemesterIndex(value.semester)
         const previousIndex = index > 0 ? index - 1 : 0
+
         return { semester: period[previousIndex] }
       })
     },
-    setCurrentIndex: (index) => set(() => ({ currentIndex: index})),
     setNextSemester: () => {
       set((value) => {
         const index = value.getSemesterIndex(value.semester)
         const n = period.length - 1
         const nextIndex = index < n ? index + 1 : n
+
         return { semester: period[nextIndex] }
       })
     },
+    getIndexList: (index, range) => {
+      let start = Math.max(0, index - range)
+      let end = Math.min(period.length - 1, index + range)
+
+      if (index < range) {
+        end = range * 2
+      }
+      if (index >= period.length - range) {
+        start = (period.length - 1) - range * 2
+      }
+      
+      return period.slice(start, end + 1)
+    },
     getSemesterIndex: (semester) => {
       const index = period.findIndex((p) => p === semester)
-      return index !== -1 ? index : 0
+      
+      return index
     },
   })),
 )
