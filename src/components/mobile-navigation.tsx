8d435d766cbf867d7b74e25e0bde230f271e49f1
ajin <aj5210@naver.com>
2024-02-26 23:12:59 +0900

    :coffin: Refactor(header/mobileNavigation): 기능 분리로 필요없는 코드 삭제

--- src/components/mobile-navigation.tsx
+++ src/components/mobile-navigation.tsx
@@ -1,10 +1,8 @@
 'use client'
 
 import Link from 'next/link'
-import { useEffect, useRef } from 'react'
 import { FaBars } from 'react-icons/fa'
 
-import { useMenuStore } from '@/store/menu'
 import { navLinks } from '@/lib/data'
 import {
   DropdownMenu,
@@ -14,29 +12,12 @@ import {
 } from '@/components/ui/dropdown-menu'
 
 export const MobileNavigation = () => {
-  const { setOpen } = useMenuStore()
-  const ref = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const closeMenu = (e: MouseEvent) => {
-      if (!e.target) return
-      if (ref.current && !ref.current.contains(e.target as Node)) {
-        setOpen(false)
-      }
-    }
-
-    document.addEventListener('mousedown', closeMenu)
-    return () => {
-      document.addEventListener('mousedown', closeMenu)
-    }
-  }, [setOpen])
-
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <FaBars onClick={() => setOpen(!open)} size="24" />
+        <FaBars size="24" />
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="mt-4" ref={ref}>
+      <DropdownMenuContent className="mt-4">
         {navLinks.map((route) => (
           <DropdownMenuItem key={route.href}>
             <Link href={route.href}>{route.name}</Link>
