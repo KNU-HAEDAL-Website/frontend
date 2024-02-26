4c110297a62f1ad562f7098c05e9eb16b087f127
ajin <aj5210@naver.com>
2024-02-26 18:14:14 +0900

    :sparkles: Feat(auth/register): 추가 회원가입 입력창 유효성 검사
    zod사용

--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -1,7 +1,11 @@
 'use client'
 
+import * as z from 'zod'
+import Link from 'next/link'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
+import { RegisterSchema } from '@/schema'
 import {
   Form,
   FormControl,
@@ -13,31 +17,23 @@ import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Checkbox } from '@/components/ui/checkbox'
 
-import Link from 'next/link'
-
 export const RegisterForm = () => {
-  const form = useForm({
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
     defaultValues: {
       name: '',
       studentId: '',
-      checked: false
+      checked: false,
     },
   })
 
-  const onSubmit = (values: {
-    name:string, 
-    studentId:string, 
-    checked:boolean
-  }) => {
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
     console.log(values)
   }
 
   return (
     <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="w-full space-y-2"
-      >
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-2">
         <FormField
           control={form.control}
           name="name"
@@ -57,39 +53,33 @@ export const RegisterForm = () => {
             <FormItem>
               <FormLabel>학번을 입력하세요</FormLabel>
               <FormControl>
-                <Input placeholder="2020123456" type="number" {...field} />
+                <Input placeholder="2024123456" type="number" {...field} />
               </FormControl>
             </FormItem>
           )}
         />
         <FormField
           control={form.control}
-          name='checked'
+          name="checked"
           render={({ field }) => (
             <FormItem>
-              <FormControl className='pt-4 flex items-center gap-2'>
+              <FormControl className="pt-4 flex items-center gap-2">
                 <div>
                   <Checkbox
                     checked={field.value}
                     onCheckedChange={field.onChange}
                   />
-                  <FormLabel>
-                    관리자의 승인 후 회원가입이 완료됩니다.
-                  </FormLabel>
+                  <FormLabel>관리자의 승인 후 회원가입이 완료됩니다.</FormLabel>
                 </div>
               </FormControl>
             </FormItem>
           )}
         />
-        <Button type='submit' className='w-full'>
-            회원가입
+        <Button type="submit" className="w-full">
+          회원가입
         </Button>
-        <Button
-          size='sm'
-          variant='link'
-          className='w-full font-normal'
-        >
-          <Link href='/auth/login'>로그인하기</Link>
+        <Button size="sm" variant="link" className="w-full font-normal">
+          <Link href="/auth/login">로그인하기</Link>
         </Button>
       </form>
     </Form>
