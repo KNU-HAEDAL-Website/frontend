d849524cd4bdd57ba7717bbcce4a8cdc3891ea4b
ajin <aj5210@naver.com>
2024-03-06 15:57:27 +0900

    :recycle: Refactor(menuLayout): 메뉴 레이아웃에서 학기 선택 컴포넌트 분리
    router로 depth확인 후 조건부 렌더링 시도했지만 SSG, SSR 렌더링 불가로 레이아웃에서 컴포넌트 분리함. closed: #32

--- src/app/(main)/(menu)/layout.tsx
+++ src/app/(main)/(menu)/layout.tsx
@@ -1,9 +1,6 @@
-import { SemesterList } from '@/components/semester-list'
-
 const MenuLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <SemesterList />
       <main className="h-full">{children}</main>
     </div>
   )
