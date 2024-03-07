6f9a8975171aa789d20324ba589670f2ed6c4824
ajin <aj5210@naver.com>
2024-03-07 11:04:40 +0900

    :sparkles: Feat(createBoard): 추가 게시판 추가 정보 입력 기능
    게시판 추가 정보(제목, 소개, 대표 사진)
    Related to: #30

--- src/app/(main)/(menu)/activity/createBoard/page.tsx
+++ src/app/(main)/(menu)/activity/createBoard/page.tsx
@@ -1,10 +1,14 @@
+'use client'
 import { BoardInfomation } from '@/components/createBoard/board-infomation'
+import { BoardInput } from '@/components/createBoard/board-input'
 
 const CreateBoardPage = () => {
+
   return (
     <div className="max-w-screen-xl xl:mx-auto">
       <div className="py-6 bg-red-200">테스트용</div>
       <BoardInfomation />
+      <BoardInput />
     </div>
   )
 }
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -13,3 +13,13 @@ export const RegisterSchema = z.object({
     }),
   }),
 })
+
+export const BoardSchema = z.object({
+  title: z.string().max(30, {
+    message: '게시판 제목은 30자 이내이어야 합니다.'
+  }),
+  intro: z.string(),
+  image: z.instanceof(File).refine((f) => f.size < 5000000, {
+    message: '파일 크기는 5MB 이하만 가능합니다.'
+  }),
+})
