eda8a79b00b05019cfeb142d29301acdcc119eac
Dobbymin <gmin0701@knu.ac.kr>
2024-03-07 09:34:09 +0900

    ✨ Feat(mainpage/activity): 추가 활동 사진 좌우 스크롤 기능
    carousel ui를 활용해 활동사진이 좌우 버튼을 이용해 움직을 수 있게 구현
    
    Ref: #22

--- src/components/main/activity-viewer.tsx
+++ src/components/main/activity-viewer.tsx
@@ -1,15 +1,41 @@
-import React from 'react'
+import * as React from 'react'
 
-import { ActivityCard } from '@/components/main/activity-card'
+import { Card, CardContent } from '@/components/ui/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+
+import { ActivityCard } from './activity-card'
 
 export function ActivityViewer() {
   const activityTitles = ['트랙', '해커톤', '부트캠프', '아이디어톤']
-
   return (
-    <div className="p-3 justify-center items-center gap-5 flex">
-      {activityTitles.map((title, index) => (
-        <ActivityCard key={index} title={title} />
-      ))}
-    </div>
+    <Carousel
+      opts={{
+        align: 'start',
+      }}
+      className="w-[1000px] max-w-[1280px]"
+    >
+      <CarouselContent>
+        {activityTitles.map((title, index) => (
+          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+            <div className="p-1">
+              <Card>
+                {/* <CardContent className="flex aspect-square items-center justify-center p-6"> */}
+                {/* <span className="text-3xl font-semibold">{title}</span> */}
+                {/* </CardContent> */}
+                <ActivityCard key={index} title={title} />
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
   )
 }
