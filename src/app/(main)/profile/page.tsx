db7636372c334fd24cf4ae6f68ece7f7094125be
Dobbymin <gmin0701@knu.ac.kr>
2024-03-10 19:28:27 +0900

    ✨ Feat(Mypage/design): 추가 마이페이지
    마이페이지 구현 및 컴포넌트화 진행
    모바일 반응형 디자인 구현
    
    Ref: #37

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,5 +1,19 @@
+import { MypageInfo } from '@/components/mypage/mypage-info'
+import { MypageOverview } from '@/components/mypage/mypage-overview'
+import { MypageProfile } from '@/components/mypage/mypage-profile'
+
 const ProfilePage = () => {
-  return <div>profile</div>
+  return (
+    <div className="w-full h-fit px-8 md:px-[120px] md:py-[100px] flex-col justify-start items-center md:gap-20 gap-5 inline-flex">
+      <div className="self-stretch bg-white justify-start items-start gap-[30px] inline-flex">
+        <MypageProfile />
+        <MypageInfo />
+      </div>
+      <div className="self-stretch h-[392px] bg-white flex-col justify-center items-center gap-10 flex">
+        <MypageOverview />
+      </div>
+    </div>
+  )
 }
 
 export default ProfilePage
