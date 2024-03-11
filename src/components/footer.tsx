64ca2a39b8040c93f464c7cd4a72a049c87a4154
Dobbymin <gmin0701@knu.ac.kr>
2024-03-11 20:45:53 +0900

    🐛 Fix(footer): 수정 모바일 반응형 디자인 적용
    width와 height, padding값 조절로 모바일에서 부모에 따라 크기가 변할 수 있게 수정
    
    Ref: #40

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,8 +6,8 @@ import { FooterText } from '@/components/footer-text'
 
 export const Footer = () => {
   return (
-    <div className="w-full h-[294px] px-[84px] py-[72px] bg-primary flex-col justify-center items-center gap-2.5 inline-flex">
-      <div className="self-stretch grow shrink basis-0 flex-col justify-between items-start flex">
+    <div className="w-full md:h-[294px] h-auto md:px-[84px] px-14 md:py-[72px] py-12 bg-primary flex-col justify-center items-center gap-2.5 inline-flex">
+      <div className="self-stretch grow shrink basis-0 flex-col md:justify-between gap-5 items-start flex">
         <FooterImage />
         <div className="self-stretch justify-between items-end inline-flex">
           <FooterText />
