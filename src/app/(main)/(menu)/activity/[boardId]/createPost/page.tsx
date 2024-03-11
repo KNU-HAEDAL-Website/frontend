e5dfbc3769d615f42abb1c760c00f586650e86e1
ajin <aj5210@naver.com>
2024-03-11 14:54:24 +0900

    :lipstick: Design(community/createPost): 수정 클릭 가능한 페이지헤더
    Related to: #51

--- src/app/(main)/(menu)/activity/[boardId]/createPost/page.tsx
+++ src/app/(main)/(menu)/activity/[boardId]/createPost/page.tsx
@@ -1,5 +1,18 @@
-const CreatePostPage = () => {
-  return <div className="max-w-screen-xl xl:mx-auto">게시글 생성하기</div>
+import { PageHeader } from '@/components/community/page-header'
+import { boardDB } from '@/lib/data'
+
+const CreatePostPage = ({ params }: { params: { boardId: string } }) => {
+  const board = boardDB.find((board) => board.id === Number(params.boardId))
+
+  return (
+    <div>
+      <PageHeader
+        navName="Activity"
+        pageName={`${board?.name} 게시판`}
+        pageLink={`/activity/${board?.id}`}
+      />
+    </div>
+  )
 }
 
 export default CreatePostPage
