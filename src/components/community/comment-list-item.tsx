9dd7a86f7db0b98fd13e1f8830b852a3a7e12488
ajin <aj5210@naver.com>
2024-03-29 18:10:03 +0900

    :lipstick: Design(community/comment): 추가 답글, 삭제, 수정 버튼 UI
    Related to: #62

--- src/components/community/comment-list-item.tsx
+++ src/components/community/comment-list-item.tsx
@@ -1,4 +1,5 @@
 import { format } from 'date-fns'
+import { Pencil, Trash2 } from 'lucide-react'
 
 import { ProfileAvatar } from '@/components/profile-avatar'
 
@@ -9,14 +10,25 @@ interface commentListItemProps {
 export const CommentListItem = ({ item }: commentListItemProps) => {
   return (
     <div key={item.id}>
-      <div className="flex flex-row gap-3">
-        <ProfileAvatar />
-        <div className="flex flex-col pb-1">
-          <div>{item.user}</div>
-          <div className="text-xs">{format(item.createdAt, 'yyyy.LL.dd')}</div>
+      <div className="flex justify-between">
+        <div className="flex flex-row gap-3">
+          <ProfileAvatar />
+          <div className="flex flex-col pb-1">
+            <div>{item.user}</div>
+            <div className="text-xs">
+              {format(item.createdAt, 'yyyy.LL.dd')}
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row gap-2 text-primary/70">
+          <Pencil className="h-4 w-4 hover:text-primary hover:cursor-pointer" />
+          <Trash2 className="h-4 w-4 hover:text-primary hover:cursor-pointer" />
         </div>
       </div>
       <div className="px-1">{item.content}</div>
+      <div className="px-1 py-2 font-normal text-sm text-primary/70 underline-offset-4 hover:text-primary hover:underline hover:cursor-pointer">
+        답글 달기
+      </div>
     </div>
   )
 }
