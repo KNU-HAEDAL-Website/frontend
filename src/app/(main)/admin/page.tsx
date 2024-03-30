b27c71ad593979647a86fb2cd1dc53b79bf30aa8
ajin <aj5210@naver.com>
2024-03-30 14:44:39 +0900

    :lipstick: Design(admin): 추가 관리자 아이템 레이아웃
    Related to: #64

---
+++ src/app/(main)/admin/page.tsx
@@ -0,0 +1,13 @@
+import { ItemLayout } from "./_components/item-layout"
+
+const AdminPage = () => {
+  return (
+    <div className="h-screen pt-16 max-w-screen-xl xl:mx-auto">
+      <ItemLayout title="test">
+        test
+      </ItemLayout>
+    </div>
+  )
+}
+
+export default AdminPage
