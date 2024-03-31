66efada0cf213bf29963b44b9cf3b734e12969d8
ajin <aj5210@naver.com>
2024-03-31 14:50:15 +0900

    :recycle: Refactor(admin/memeber/grade): grade-dialog,grade-dialog-content 컴포넌트 병합
    Related to: #70

--- src/app/(main)/admin/_components/member/grade-dialog.tsx
+++ src/app/(main)/admin/_components/member/grade-dialog.tsx
@@ -1,7 +1,13 @@
 import { Button } from '@/components/ui/button'
-import { Dialog, DialogTrigger } from '@/components/ui/dialog'
-
-import { GradeDialogContent } from './grade-dialog-content'
+import { 
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '@/components/ui/dialog'
+  
+import { GradeRadioBox } from './grade-radio-box'
 
 interface GradeDialogProps {
   user: ManageUserGrade
@@ -15,7 +21,17 @@ export const GradeDialog = ({ user }: GradeDialogProps) => {
           {user.grade}
         </Button>
       </DialogTrigger>
-      <GradeDialogContent user={user} />
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle className="flex gap-2 text-md">
+            <span>
+              {user.name}({user.studentId})
+            </span>
+            <span>권한 설정</span>
+          </DialogTitle>
+        </DialogHeader>
+        <GradeRadioBox user={user} />
+      </DialogContent>
     </Dialog>
   )
 }
