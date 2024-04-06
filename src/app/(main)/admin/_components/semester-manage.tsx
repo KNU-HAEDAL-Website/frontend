d8714df492a34fb51da90f603ed0d1c44043b144
ajin <aj5210@naver.com>
2024-04-06 21:51:08 +0900

    :lipstick: Design(admin/semester): 추가 학기 아이템 박스 가로 스크롤
    Related to: #85

--- src/app/(main)/admin/_components/semester-manage.tsx
+++ src/app/(main)/admin/_components/semester-manage.tsx
@@ -7,13 +7,17 @@ import { SemesterBox } from './semester/semester-box'
 export const SemesterManage = () => {
   return (
     <ItemLayout title="학기 관리">
-      <div className="flex gap-2">
-        {semesterDB.map((semester) => (
-          <SemesterBox key={semester} className="select-none">
-            {semester}
-          </SemesterBox>
-        ))}
-        <AddDialog />
+      <div className="flex flex-row gap-2">
+        <div className="w-9 h-9">
+          <AddDialog />
+        </div>
+        <div className="flex flex-row gap-1 overflow-scroll">
+          {semesterDB.map((semester) => (
+            <SemesterBox key={semester} className="select-none">
+              {semester}
+            </SemesterBox>
+          ))}
+        </div>
       </div>
     </ItemLayout>
   )
--- src/app/(main)/admin/_components/semester/semester-box.tsx
+++ src/app/(main)/admin/_components/semester/semester-box.tsx
@@ -11,12 +11,12 @@ export const SemesterBox = React.forwardRef<
     <div
       ref={ref}
       className={cn(
-        'flex justify-center items-center gap-2 rounded-3xl px-3 py-2 text-sm font-medium transition-all bg-primary text-white hover:bg-primary/90 hover:cursor-grab',
+        'flex justify-center items-center rounded-3xl h-full px-3 text-sm font-regular transition-all bg-primary text-white hover:bg-primary/90 hover:cursor-grab',
         className,
       )}
       {...props}
     >
-      <div>{children}</div>
+      <div className="w-14">{children}</div>
       <div className="w-5 h-5 flex items-center justify-center">
         <X className="w-4 h-4 transition-all hover:cursor-pointer hover:w-5 hover:h-5" />
       </div>
