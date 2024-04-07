103da697aa150c2b62785f62a99f82b4e176fbc4
ajin <aj5210@naver.com>
2024-04-07 23:01:21 +0900

    :sparkles: Feat(auth/login): 추가 로그인 Form
    Related to: #88

---
+++ src/app/auth/login/_components/login-form.tsx
@@ -0,0 +1,69 @@
+'use client'
+
+import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { LoginSchema } from '@/schema'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  })
+
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-2">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>아이디를 입력하세요</FormLabel>
+              <FormControl>
+                <Input placeholder="hobanu" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>비밀번호를 입력하세요</FormLabel>
+              <FormControl>
+                <Input placeholder="******" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          로그인하기
+        </Button>
+      </form>
+    </Form>
+  )
+}
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,10 @@
 import { AuthLayout } from '../_components/auth-layout'
+import { LoginForm } from './_components/login-form'
 
 const LoginPage = () => {
   return (
     <AuthLayout headerTitle="Sign up to HAEDAL">
-      test
+      <LoginForm />
     </AuthLayout>
   )
 }
