2669bf8c3fcd387e5bb9cfe46cb11f9d8f40bc73
ajin <aj5210@naver.com>
2024-04-10 23:19:04 +0900

    :recycle: Refactor(login): 로그인 Form 컴포넌트 분리
    Related to: #88

--- src/app/auth/login/_components/login-form.tsx
+++ src/app/auth/login/_components/login-form.tsx
@@ -5,28 +5,17 @@ import { useForm } from 'react-hook-form'
 
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-
-import { LoginSchema } from '@/schema'
 import { login } from '@/actions/login'
+import { LoginSchema } from '@/schema'
 import { FormError } from '@/components/form-error'
-import { FormSuccess } from '@/components/form-success'
 import { Button } from '@/components/ui/button'
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
+import { Form, FormField } from '@/components/ui/form'
 
-import { BackButton } from '../../_components/back-button'
+import { FormInput } from '../../_components/form-input'
 
 export const LoginForm = () => {
   const [isPending, startTransition] = useTransition()
   const [error, setError] = useState<string | undefined>('')
-  const [success, setSuccess] = useState<string | undefined>('')
 
   const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
@@ -38,56 +27,51 @@ export const LoginForm = () => {
 
   const onSubmit = (values: z.infer<typeof LoginSchema>) => {
     setError('')
-    setSuccess('')
 
     startTransition(() => {
       login(values).then((data) => {
         setError(data.error)
-        setSuccess(data.success)
       })
     })
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-2">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>아이디를 입력하세요</FormLabel>
-              <FormControl>
-                <Input placeholder="hobanu" disabled={isPending} {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>비밀번호를 입력하세요</FormLabel>
-              <FormControl>
-                <Input
-                  placeholder="******"
-                  type="password"
-                  disabled={isPending}
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        {error && <FormError message={error} />}
-        {success && <FormSuccess message={success} />}
-        <Button type="submit" className="w-full" disabled={isPending}>
-          로그인하기
-        </Button>
-        <BackButton label='회원가입하러가기' backLink='/auth/register' />
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
+        <div className="space-y-2">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormInput
+                inputLabel="아이디"
+                placehoder="hobanu"
+                isPending={isPending}
+                value={field.value}
+                onChange={field.onChange}
+              />
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormInput
+                inputLabel="비밀번호"
+                placehoder="********"
+                isPending={isPending}
+                value={field.value}
+                onChange={field.onChange}
+              />
+            )}
+          />
+        </div>
+        <div className="space-y-2">
+          {error && <FormError message={error} />}
+          <Button type="submit" className="w-full" disabled={isPending}>
+            로그인하기
+          </Button>
+        </div>
       </form>
     </Form>
   )
