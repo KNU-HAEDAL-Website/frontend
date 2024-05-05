29b55f580dcb3ede0792f9e52896ef2c071bf138
ajin <aj5210@naver.com>
2024-05-05 16:04:38 +0900

    :fire: Remove(services/checkUserId): 유효성 검사 zod로 실행함으로 기존 코드 삭제

--- src/services/checkUserId.ts
+++ src/services/checkUserId.ts
@@ -3,13 +3,6 @@
 import { backendApi } from '@/constant/api'
 
 export const checkUserId = async (userId: string) => {
-  if (userId.length < 6) {
-    return {
-      success: false,
-      message: 'ID는 6자 이상이어야 합니다.',
-    }
-  }
-
   try {
     const res = await backendApi.get(`/join/check-user-id?userId=${userId}`)
     const { success, message } = res.data
