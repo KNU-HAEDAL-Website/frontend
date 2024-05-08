a9b8b5daaec7ac30d0050fbd5f6ee464438f6fce
ajin <aj5210@naver.com>
2024-05-09 01:26:53 +0900

    :sparkles: Feat(reissue): 추가 access Token 만료 시 reissue로 재발급
    Related to: #116

--- src/constant/api.ts
+++ src/constant/api.ts
@@ -1,6 +1,16 @@
 import axios from 'axios'
+import { errorResponse } from '@/lib/error-reponse'
+import { successResponse } from '@/lib/sucssess-response'
 
-export const backendApi = axios.create({
+const backendApi = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   withCredentials: true,
 })
+
+const authorizationApi = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  withCredentials: true,
+})
+authorizationApi.interceptors.response.use(successResponse, errorResponse)
+
+export { backendApi, authorizationApi }
