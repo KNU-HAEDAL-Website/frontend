b807fb0c5e79198f11fe237824736c2269df26ce
ajin <aj5210@naver.com>
2024-05-18 23:34:42 +0900

    :lipstick: Design(member-upgrade): user 데이터 fetch 시에 없으면 빈 table 보여주기

--- src/app/(main)/admin/_components/member/upgrade-table.tsx
+++ src/app/(main)/admin/_components/member/upgrade-table.tsx
@@ -82,11 +82,8 @@ export const UpgradeTable = () => {
       {!error && (
         <>
           {data === undefined && <SkeletonTable />}
-          {data !== undefined && data?.length > 0 && (
+          {data !== undefined && (
             <MemberTable data={data} columns={columns} />
-          )}
-          {data !== undefined && data.length === 0 && (
-            <div>회원 승인 요청이 없습니다.</div>
           )}
         </>
       )}
