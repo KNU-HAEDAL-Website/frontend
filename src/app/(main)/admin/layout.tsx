358a9624ec4d5d1e037133aa8aee19b538cc0663
ajin <aj5210@naver.com>
2024-06-05 23:42:05 +0900

    :sparkles: Feat: loading UI 추가
    - client 렌더링으로 로딩 상태는 suspense에서 use state로 변경

--- src/app/(main)/admin/layout.tsx
+++ src/app/(main)/admin/layout.tsx
@@ -1,12 +1,8 @@
-import { Suspense } from 'react'
-
-import Loading from './loading'
-
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <main className="h-screen pt-16 max-w-screen-xl xl:mx-auto">
-        <Suspense fallback={<Loading />}>{children}</Suspense>
+        {children}
       </main>
     </div>
   )
