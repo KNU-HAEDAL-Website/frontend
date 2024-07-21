3c43380742c51bf7bfb9af0425b1388b8cacb016
ajin <aj5210@naver.com>
2024-07-21 15:51:22 +0900

    :sparkles: Feat: card, link button 컴포넌트 추가
    related to: #148

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,13 @@
+import { AuthCard } from '@/app/auth/_components/AuthCard'
+import { LinkButton } from '@/app/auth/_components/LinkButton'
+
 const LoginPage = () => {
-  return <div>login</div>
+  return (
+    <AuthCard title="HAEDAL Login">
+      <div>test</div>
+      <LinkButton backLink="/auth/signup" label="회원가입하기" />
+    </AuthCard>
+  )
 }
 
 export default LoginPage
