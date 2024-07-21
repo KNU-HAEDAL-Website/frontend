837581b2cfb2d54779dcf456863a2918806148be
ajin <aj5210@naver.com>
2024-07-21 16:58:15 +0900

    :sparkles: Feat: axios 기본 설정 추가
    relate to: #146

---
+++ src/service/instance.ts
@@ -0,0 +1,15 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+
+export const initInstance = (config: AxiosRequestConfig): AxiosInstance => {
+  const instance = axios.create({
+    timeout: 5000,
+    ...config,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      ...config.headers,
+    },
+  })
+
+  return instance
+}
