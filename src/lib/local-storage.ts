b6c62b99101fd5b12e360c20da5ef93517458f7b
ajin <aj5210@naver.com>
2024-07-22 15:47:18 +0900

    :sparkles: Feat: local storage 클래스 추가
    - ssr에서 안전하게 local storage에 접근 가능
    related to: #152

---
+++ src/lib/local-storage.ts
@@ -0,0 +1,24 @@
+class LocalStorage {
+  constructor() {}
+
+  static setItem(key: string, value: string) {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem(key, value)
+    }
+  }
+
+  static getItem(key: string) {
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem(key)
+    }
+    return null
+  }
+
+  static clear() {
+    if (typeof window !== 'undefined') {
+      localStorage.clear()
+    }
+  }
+}
+
+export default LocalStorage
