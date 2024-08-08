93b30f995b382e96d0d59b4be14c40cec3799246
ajin <aj5210@naver.com>
2024-08-08 11:44:36 +0900

    :bug: Fix: activity request param 네이밍 수정

--- src/service/server/activity/index.ts
+++ src/service/server/activity/index.ts
@@ -1,11 +1,13 @@
 import { BACKEND_API } from '@/service/config'
 import { Activity } from '@/types/activity'
 
-type SemesterRequestParams = {
+type ActivitiesRequestParams = {
   semesterId: number
 }
 
-export const getActivities = async ({ semesterId }: SemesterRequestParams) => {
+export const getActivities = async ({
+  semesterId,
+}: ActivitiesRequestParams) => {
   const response = await BACKEND_API.get<Activity[]>(
     `/semesters/${semesterId}/activities`,
   )
